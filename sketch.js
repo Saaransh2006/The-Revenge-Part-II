bd450a2ef83cc5f7d58db48cd34d8d2b520a2a34
Saaransh2006 <70142473+Saaransh2006@users.noreply.github.com>
2021-02-08 16:12:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,9 +23,6 @@ function preload() {
 
 //Function for setting up.
 function setup() {
-  //Looping the background music.
-  bgm.loop();
-
   //Creating the canvas area.
   createCanvas(1350, 700);
 
@@ -177,6 +174,8 @@ function draw() {
       bgm.setVolume(0.1);
       //Playing bgm.
       bgm.play();
+      //Looping the background music.
+      bgm.loop();
       //Destroying sprite3.
       sprite3.destroy();
     }
